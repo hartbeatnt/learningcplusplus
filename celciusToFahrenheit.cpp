d7fc9537f514a0c0eddc09ffa3afbbdd75bd974d
Nathaniel T. Hart <hartbeatnt@gmail.com>
2017-04-18 14:07:55 -0700

    even odd shows if/else branching

--- celciusToFahrenheit.cpp
+++ celciusToFahrenheit.cpp
@@ -6,7 +6,6 @@ int main()
   double temp;
   cout << "Input a Celsius temp\nand press ENTER: ";
   cin >> temp;
-  temp = (temp * 1.8) + 32.0;
-  cout << "Fahrenheit temp is: " << temp;
+  cout << "Fahrenheit temp is: " << temp * 1.8 + 32.0;
   return 0;
 }
